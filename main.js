ebc96a174650d635ac08043bcdd77f9ae850c89e
sheinthuaung <sheinthuaung.725@gmail.com>
2021-08-14 02:35:08 +0630

    Navigation fix overlapping text

---
+++ main.js
@@ -0,0 +1,15 @@
+// Sticky Navbar
+window.onscroll = function() {stickyFunction()};
+        
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+        
+        function stickyFunction() {
+          if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+          } else {
+            navbar.classList.remove("sticky");
+          }
+        }
+
+//Responsive Navbar
